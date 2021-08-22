e26ce14f6f32c6c777307a5e184d891e87cf93a2
David Ferrol <david.ferrol@edf-energy.com>
2021-08-22 17:48:32 +0100

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,34 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() {runApp(const MyApp());}
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  	const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+	// This widget is the root of your application.
+	@override
+	Widget build(BuildContext context) {
+		return MaterialApp(
+			title: 'Flutter Demo',
+      		theme: ThemeData(
+        	// This is the theme of your application.
+        	//
+        	// Try running your application with "flutter run". You'll see the
+        	// application has a blue toolbar. Then, without quitting the app, try
+        	// changing the primarySwatch below to Colors.green and then invoke
+        	// "hot reload" (press "r" in the console where you ran "flutter run",
+        	// or simply save your changes to "hot reload" in a Flutter IDE).
+        	// Notice that the counter didn't reset back to zero; the application
+        	// is not restarted.
+        		primarySwatch: Colors.blue,
+      		),
+      		home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    	);
+  	}
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+	const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -41,25 +39,25 @@ class MyHomePage extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
-  final String title;
+	final String title;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+	@override
+	State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+	int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
+	void _incrementCounter() {
+    	setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+      		_counter++;
+    	});
+  	}
 
   @override
   Widget build(BuildContext context) {
